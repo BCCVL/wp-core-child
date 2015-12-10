566d875435f8935f321ea53cf2d094181c5290b5
Kerri Wait <kerri.wait@ieee.org>
2015-12-10 13:43:11 +1000

    Removed old cached files that weren't needed.

--- themes/core/parts/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div class="grid_6 grid_8_m sidebar float_left">
-	<?php the_sub_pages($has_banner); ?>
-	<?php if(is_page()) : include('recent-blog.php'); endif; ?>
-	<?php dynamic_sidebar('Page Sidebar'); ?>
-</div><!-- /.grid -->
